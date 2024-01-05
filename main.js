26b975f3fdcae7e15899eb5aa38278122f6aa4dc
farukulWD <farukgb1999@gmail.com>
2024-01-05 22:22:49 +0600

    connect babel

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-console.log("row react")
-console.log("React =>",React)
-console.log("React Dom =>",ReactDOM)
+// const HelloWorld = ()=>{
+//   return  React.createElement("p",{style:{color:"green"}},"Hello World")
+// }
+// const App = ()=>{
+//   {
+//     return  React.createElement("h1",{style:{color:"red"}},"Hello World, From fokira react app", HelloWorld())
+
+// }
+// }
+
+const HelloWorld = () => {
+  return <h2>Hello world</h2>;
+};
+
+const App = () => {
+  return(
+  <React.Fragment>
+    <div>
+      <h1>This is a fokira react app</h1>
+    </div>
+    <HelloWorld />
+  </React.Fragment>)
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
